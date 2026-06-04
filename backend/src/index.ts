aaed44a1e1487bc9002c0ad7b8f8f0294102f4fa
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-06-04 15:40:44 +0500

    try catch middleware user model creation and routes file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/user.routes.js";
+
+dotenv.config();
+connectDB().then(() => console.log("DB connected"));
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use("/api/user", userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
