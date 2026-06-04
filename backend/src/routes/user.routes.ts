9495cb04c9d22c9cfe9bf432425aa71f02921ad4
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-06-04 17:50:41 +0500

    routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
+import { loginUser, myProfile } from "../controllers/user.controller.js";
+import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
-router.post("/login",);
-router.get("/me",);
+router.post("/login", loginUser);
+router.get("/me", isAuth, myProfile);
 
 export default router;
